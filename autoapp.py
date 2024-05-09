24158a5b2f1a4b26c2c7e2a3ab75e61cb57cf349
Harshal Honde <86480339+Harry262000@users.noreply.github.com>
2024-05-09 23:28:19 +0530

    update autoapp.py

--- autoapp.py
+++ autoapp.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-"""Create an application instance."""
-from Flask.app import create_app
+# # -*- coding: utf-8 -*-
+# """Create an application instance."""
+# from Flask.app import create_app
 
-app = create_app()
+# app = create_app()
